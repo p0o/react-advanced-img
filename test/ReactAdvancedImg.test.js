d9ab56554d99671998bb2a9dff2b9ec69106d12c
p0o <pooria_az@ymail.com>
2017-01-05 01:27:20 +0800

    write basic test to fail

--- test/ReactAdvancedImg.test.js
+++ test/ReactAdvancedImg.test.js
@@ -3,9 +3,7 @@ import test from 'ava';
 import { shallow } from 'enzyme';
 import Img from '../src/ReactAdvancedImg.js';
 
-test('Render component', (t) => {
-  let click = () => {};
+test('Render img component', (t) => {
   let wrapper = shallow(<Img src="something" />);
-  console.log(wrapper.find('img'))
-  t.is(wrapper.find('img'), 2);
+  t.is(wrapper.find('img').length, 1);
 });
