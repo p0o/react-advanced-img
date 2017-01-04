a5afc06dd35316f76f57daff9d04da824e3a8ef7
p0o <pooria_az@ymail.com>
2017-01-05 02:30:55 +0800

    make placeholder to load in case of error

--- src/ReactAdvancedImg.js
+++ src/ReactAdvancedImg.js
@@ -1,6 +1,6 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-export default class ReactAdvancedImg extends React.Component {
+export default class ReactAdvancedImg extends Component {
   static propTypes = {
     src: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
@@ -8,13 +8,41 @@ export default class ReactAdvancedImg extends React.Component {
     className: PropTypes.string,
   };
 
+  static defaultProps = {
+    style: {},
+    className: '',
+    placeholder: 'placeholder',
+    width: 500,
+    height: 500
+  };
+
   constructor(props) {
-      super(props);
+    super(props);
+    this.state = {
+      loadPlaceholder: false,
+    };
   }
 
+  handleError = () => {
+    this.setState({ loadPlaceholder: true });
+  };
+
   render() {
+    if (this.state.loadPlaceholder) {
+      const { width, height, placeholder } = this.props;
+      const placeholderUrl = `https://dummyimage.com/${width}x${height}/d1cfd1/fff&text=${placeholder}`;
+      return (
+        <img
+          src={placeholderUrl}
+        />
+      );
+    }
+
     return (
-      <p>{this.props.src}</p>
+      <img
+        src={this.props.src}
+        onError={this.handleError}
+      />
     );
   }
 };
