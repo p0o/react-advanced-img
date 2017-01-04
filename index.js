d0f47226ca8a13a88a898155dee20e62f1227903
p0o <pooria_az@ymail.com>
2017-01-05 02:29:59 +0800

    add good and bad image to render

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import ReactDom from 'react-dom';
 import Img from './src/ReactAdvancedImg.js';
 
 ReactDom.render(
-  <Img src="something" />,
+  <div>
+    <h2>Bad Image:</h2>
+    <Img src="http://wrongurl" />
+    <h2>Good Image:</h2>
+    <Img src="http://www.thereformat.com/images/js4560_450.png" />
+  </div>,
   document.getElementById('root')
 );
